65a0eb6cafb12ca2f20d1816f1e58e720d5179ca
Wangquanjin-clould <wangquanjin@gmail.com>
2025-11-30 21:24:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,14 +26,26 @@ sed -i '/modem-power/,/};/{s/gpio-export,output = <1>;/gpio-export,output = <0>;
 sed -i -e '/^IMG_PREFIX:=/i BUILD_DATE := $(shell date +%Y%m%d)' \
        -e '/^IMG_PREFIX:=/ s/\($(SUBTARGET)\)/\1-$(BUILD_DATE)/' include/image.mk
 
-# set ubi to 122M
-# sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
+set ubi to 122M
+sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
 
-# Add OpenClash Meta
-mkdir -p files/etc/openclash/core
+# 创建存储二进制文件的目录
+BIN_DIR="$GITHUB_WORKSPACE/openwrt/files/usr/bin"
+mkdir -p "$BIN_DIR"
 
-wget -qO "clash_meta.tar.gz" "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
-tar -zxvf "clash_meta.tar.gz" -C files/etc/openclash/core/
-mv files/etc/openclash/core/clash files/etc/openclash/core/clash_meta
-chmod +x files/etc/openclash/core/clash_meta
-rm -f "clash_meta.tar.gz"
+# -------- 下载并解压 xray-core ARM64 -------
+echo "Downloading xray-core..."
+curl -L -o xray.zip https://github.com/XTLS/Xray-core/releases/download/v25.10.15/Xray-linux-arm64-v8a.zip
+unzip -o xray.zip -d "$BIN_DIR"
+chmod +x "$BIN_DIR/xray"
+rm xray.zip
+
+# -------- 下载并解压 sing-box ARM64 -------
+echo "Downloading sing-box..."
+curl -L -o sing-box.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.12.12/sing-box-1.12.12-linux-arm64.tar.gz
+TMP_DIR=$(mktemp -d)
+tar -xzf sing-box.tar.gz -C "$TMP_DIR"
+mv "$TMP_DIR"/sing-box-1.12.12-linux-arm64/sing-box "$BIN_DIR"/sing-box
+chmod +x "$BIN_DIR/sing-box"
+rm -rf "$TMP_DIR"
+rm sing-box.tar.gz
